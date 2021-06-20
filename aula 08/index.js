5653ebce42a3f5ee57ee1eca72cfcc706febbd11
Felipe Santos <dev@luisfelipesantos.com.br>
2021-06-20 19:13:25 -0300

    add aula 08

---
+++ aula 08/index.js
@@ -0,0 +1,9 @@
+const nome = 'Felipe';
+const sobrenome = 'Santos';
+const idade = 30;
+const peso = 100;
+const altura = 1.76;
+
+let imc = peso / altura;
+
+  console.log(`${nome} ${sobrenome} tem ${idade} anos. pesa ${peso}kg e tem ${altura} e seu IMC é de ${imc}`)
